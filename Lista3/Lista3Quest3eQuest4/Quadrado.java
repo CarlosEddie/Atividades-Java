8e80e5b6fe05e16deb011b4ec58922d71482e68e
Carlos Eduardo <67875731+CarlosEddie@users.noreply.github.com>
2020-07-15 17:37:26 -0300

    Update Quadrado.java

--- Lista3/Lista3Quest3eQuest4/Quadrado.java
+++ Lista3/Lista3Quest3eQuest4/Quadrado.java
@@ -3,16 +3,16 @@ public class Quadrado {
 	
 	public double lado;
 
-    public Quadrado(double lado) {
-        this.lado = lado;
-    }
+    	public Quadrado(double lado) {
+        	this.lado = lado;
+    	}
     
-    public double area() {
-        double area = this.lado * this.lado;
+	public double area() {
+        	double area = this.lado * this.lado;
         return (area);
-    }
-    public double perimetro() {
-        double perimetro = 4 * this.lado;
+    	}
+    	public double perimetro() {
+        	double perimetro = 4 * this.lado;
         return (perimetro);
     }
     
